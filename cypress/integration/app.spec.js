96bf4e4960d644105329b9723c1ec9df9090251a
timwis <tim@timwis.com>
2022-01-18 07:31:01 +0000

    Use fixtures for cypress test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -4,11 +4,22 @@
 describe('split the distance', () => {
   context('basic search', () => {
     it('searches', () => {
+      cy.intercept('/geocoding/v5/mapbox.places/N1%20.json?*', { fixture: 'location-input.json' })
+        .as('getLocations')
+
+      cy.intercept('POST', '/traveltime/time-map', { fixture: 'time-map.json' })
+        .as('getTimeMap')
+
+      cy.intercept('/foursquare/places/search?*', { fixture: 'coffee-shops.json' })
+        .as('getCoffeeShops')
+
       cy.visit('/search')
 
       cy.get('[data-testid=yourLocation')
         .find('input')
-        .type('N1 9AF')
+        .type('N1 ')
+
+      cy.wait('@getLocations')
 
       cy.get('[data-testid=yourLocation]')
         .find('.dropdown-menu')
@@ -17,15 +28,19 @@ describe('split the distance', () => {
 
       cy.get('[data-testid=theirLocation]')
         .find('input')
-        .type('CR0 1LF')
+        .type('N1 ')
+
+      cy.wait('@getLocations')
 
       cy.get('[data-testid=theirLocation]')
         .find('.dropdown-menu')
-        .contains('CR0 1LF, Croydon')
+        .contains('N1 8EQ, London')
         .click()
 
       cy.get('[data-testid=search-btn]')
         .click()
+
+      cy.wait(['@getTimeMap', '@getCoffeeShops'])
 
       cy.url().should('include', '/results')
     })
